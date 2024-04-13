d90e207fef386406bbda1ff5a1a161606c943ab7
Sohil-Koitiya <shlkoitiya@gmail.com>
2024-04-13 19:15:29 +0530

    JQuery Assessment Resubmit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-$(document).ready(function () {
-    
-    $("#nav").click(function () {
-    $("#nav").css({
-        backgroundColor: "lightblue",
-        color: "blue"
-    })
-    })
-})
+sp_tag = document.getElementById('bars');
+ul_tag = document.getElementById('menu');
+
+
+
+function getMenu()
+{ 
+
+    if(ul_tag.style.display =="none")
+    {
+        ul_tag.style.display ="block"
+    }
+    else 
+    {
+        ul_tag.style.display ="none"
+    }
+     
+}
